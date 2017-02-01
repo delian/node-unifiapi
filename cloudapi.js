19a7377a141cf95038cc453699ddce5c133db501
Delian Delchev <delian.delchev@gmail.com>
2017-02-01 21:34:18 +0100

    small updates to the api
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- cloudapi.js
+++ cloudapi.js
@@ -1,6 +1,7 @@
 let debug = require('debug')('CloudAPI');
 let merge = require('merge');
 let CloudRequest = require('./lib/cloud-request');
+let wss = require('./lib/unifi-wss');
 
 let defaultOptions = {
     'username': 'unifi',
@@ -12,7 +13,7 @@ let defaultOptions = {
     'site': 'default'
 };
 
-function CloudAPI (options) {
+function CloudAPI(options) {
     if (!(this instanceof CloudAPI)) return new CloudAPI(options);
     merge(this, defaultOptions, options);
     if (this.debug) debug.enabled = true;
@@ -36,18 +37,53 @@ CloudAPI.prototype.self = function() {
 
 CloudAPI.prototype.devices = function() {
     return this.net.req('/devices', undefined, undefined, undefined,
-        baseUrl="https://device-airos.svc.ubnt.com/api/airos/v1/unifi");
+        baseUrl = "https://device-airos.svc.ubnt.com/api/airos/v1/unifi");
 };
 
 CloudAPI.prototype.delete_device = function(device_id = '') {
-    return this.net.req('/devices/'+device_id, undefined, undefined, method = 'DELETE',
-        baseUrl="https://device-airos.svc.ubnt.com/api/airos/v1/unifi");
+    return this.net.req('/devices/' + device_id, undefined, undefined, method = 'DELETE',
+        baseUrl = "https://device-airos.svc.ubnt.com/api/airos/v1/unifi");
 };
 
 CloudAPI.prototype.turn_creds = function(device_id) {
-    return this.net.req('/turn/creds?username='+device_id, undefined, undefined, undefined,
-        baseUrl="https://device-airos.svc.ubnt.com/api/airos/v1/unifi"
+    return this.net.req('/turn/creds?username=' + device_id, undefined, undefined, undefined,
+        baseUrl = "https://device-airos.svc.ubnt.com/api/airos/v1/unifi"
     );
+};
+
+CloudAPI.prototype.openWebRtc = function(device_id) {
+    return new Promise((resolve, reject) => {
+        this.login()
+            .then(() => {
+                let cookie = this.net.jar.getCookieString(this.baseUrl);
+                this.wss = wss({
+                    debug: this.debug,
+                    cookie: cookie
+                });
+                return this.wss.connect();
+            })
+            .then(() => {
+                debug('WebSocket is connected');
+                return this.turn_creds(device_id);
+            })
+            .then((data) => {
+                let stunUri = data.uris.filter((n) => n.match(/^stun/)).shift();
+                let turnUri = data.uris.filter((n) => n.match(/^turn/)).shift();
+                return this.wss.actionRequest('sdp_exchange', {
+                    device_id: device_id,
+                    payload: {
+                        username: data.username,
+                        password: data.password,
+                        ttl: 86400,
+                        type: 'OFFER',
+                        stunUri: stunUri,
+                        turnUri: turnUri
+                    }
+                });
+            })
+            .then(resolve)
+            .catch(reject);
+    });
 };
 
 module.exports = CloudAPI;
