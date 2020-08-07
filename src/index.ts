230ba203f83ee76acf5b55dcd82ec06fabf1464e
Delian Delchev <delian.delchev@gmail.com>
2020-08-07 17:38:28 +0200

    wait ported
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /** @format */
 
 import { debug } from 'debug';
+import { merge } from 'merge';
+import { UnifiRequest } from './lib/unifi-request';
+import { SSHSession } from './lib/ssh-session';
 
-console.log('aaa');
---
+++ src/lib/wait.ts
@@ -0,0 +1,14 @@
+/** @format */
+
+/**
+ * Asynchronous wait implementation
+ * @param {number} timeout the timeout to wait in miliseconds
+ * @param {any} data optional data to return after the wait
+ */
+export function wait(timeout: number = 100, data: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(data);
+        }, timeout);
+    });
+}
